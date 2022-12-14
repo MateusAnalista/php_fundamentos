8d642b667edf5659ceb71f0059783913c856a676
Mateus Moraes <mmoraes@Mateuss-MacBook-Pro.local>
2022-12-14 11:13:20 -0300

    first commit

---
+++ 02_tipos_de_impressao.php
@@ -0,0 +1,11 @@
+<?php
+
+// echo '123';
+
+// print 'teste';
+
+$varArray = [1,2,3,4,5];
+
+print_r($varArray);
+
+// print_r('teste');
---
+++ 03_tipo_comentarios.php
@@ -0,0 +1,11 @@
+<?php
+
+// echo ('1'); aqui esta 1 comentario de 1 linha
+echo ('2');
+echo ('3');
+
+// aqui esta um comentario de bloco
+/*
+echo ('4');
+*/
+
